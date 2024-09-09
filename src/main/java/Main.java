810a721152883021129a29e8b657f0de8ce05b49
rigo0809 <169068411+rigo0809@users.noreply.github.com>
2024-09-09 01:38:04 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    
+  
+
+  String nome = "isa";
+  int dia = 8;    
+  int mes = 9;
+  int ano = 2008;
+  double pi = 3.141592; 
+
+  System.out.printf(" o valor de pi e: %.2f",pi );
+  }
+}
